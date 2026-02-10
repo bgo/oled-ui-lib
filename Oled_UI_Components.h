c28c8ad8a593249b14acb8e7be523ac9cb85099b
bgo <bugraokcu@gmail.com>
2026-02-11 02:53:48 +0300

    comply with lib specs

---
+++ Oled_UI_Components.h
@@ -0,0 +1,25 @@
+#ifndef OLED_UI_COMPONENTS_H
+#define OLED_UI_COMPONENTS_H
+
+#include "UIComponent.h"
+#include "UIRenderer.h"
+
+#include "UIButton.h"
+#include "UICheckbox.h"
+#include "UIDropdownMenu.h"
+#include "UIImage.h"
+#include "UILabel.h"
+#include "UILineChart.h"
+#include "UIList.h"
+#include "UIMessageBox.h"
+#include "UINavbar.h"
+#include "UINumericInput.h"
+#include "UIProgressBar.h"
+#include "UIRadioButton.h"
+#include "UISeparator.h"
+#include "UISlider.h"
+#include "UISpinner.h"
+#include "UITextInput.h"
+#include "UIToggleSwitch.h"
+
+#endif
